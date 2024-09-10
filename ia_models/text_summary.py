9780c6fd5e73919568d160ab359e71de321e025a
AndreAloise <andre.aloise.77@gmail.com>
2024-09-09 21:45:53 -0300

    Creating summary model

---
+++ ia_models/text_summary.py
@@ -0,0 +1,14 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+class TextSummary:
+	def __init__(self):
+		model_name = "csebuetnlp/mT5_multilingual_XLSum"
+		self._tokenizer = AutoTokenizer.from_pretrained(model_name)
+		self._model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+	def get_tokenizer(self):
+		return self._tokenizer
+
+	def get_model(self):
+			return self._model

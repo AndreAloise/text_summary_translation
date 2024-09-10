821c5b60423ae1dec9d534487720b869b5096e59
AndreAloise <andre.aloise.77@gmail.com>
2024-09-10 02:50:37 -0300

    Updating services with param default value

--- services/text_summary_service.py
+++ services/text_summary_service.py
@@ -1,12 +1,13 @@
-from ia_models.text_summary import TextSummary
 import re
+
+from ia_models.text_summary import TextSummary
 
 
 class TextSummaryService:
 	def __init__(self, model: TextSummary):
 		self._model = model
 
-	def summarize_text(self, text: str):
+	def summarize_text(self, text: str, output_max_length: int = 200):
 		processed_text = self._get_processed_text_without_white_space(text)
 
 		input_ids = self._model.get_tokenizer()(
@@ -19,18 +20,17 @@ class TextSummaryService:
 
 		output_ids = self._model.get_model().generate(
 			input_ids=input_ids,
-			max_length=200,
+			max_length=output_max_length,
 			no_repeat_ngram_size=2,
 			num_beams=4
 		)[0]
 
 		summary = self._model.get_tokenizer().decode(
-		    output_ids,
-		    skip_special_tokens=True,
-		    clean_up_tokenization_spaces=False
+			output_ids,
+			skip_special_tokens=True,
+			clean_up_tokenization_spaces=False
 		)
 		return summary
-
 
 	@staticmethod
 	def _get_processed_text_without_white_space(text: str):

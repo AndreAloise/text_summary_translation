f96d33949a6960ee18f458f9baff7aab767197bf
AndreAloise <andre.aloise.77@gmail.com>
2024-09-09 23:58:46 -0300

    Changing model to facebook in 'en'

--- ia_models/text_summary.py
+++ ia_models/text_summary.py
@@ -1,9 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-
 class TextSummary:
 	def __init__(self):
-		model_name = "csebuetnlp/mT5_multilingual_XLSum"
+		model_name = "facebook/bart-large-cnn"
 		self._tokenizer = AutoTokenizer.from_pretrained(model_name)
 		self._model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 
